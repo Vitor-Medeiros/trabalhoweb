8ec8cdee1e172028fc47ed95592e651c9467b14e
Vitor-Medeiros <vitorbortot@gmail.com>
2024-09-29 23:42:22 -0300

    fim

---
+++ src/main/java/com/unimater/dao/GenericDAO.java
@@ -0,0 +1,17 @@
+package com.unimater.dao;
+
+import com.unimater.model.Entity;
+
+import java.util.List;
+
+public interface GenericDAO<T extends Entity> {
+    List<T> getAll();
+    T getById(int id);
+    void upsert(T entity);
+    void delete(int id);
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> web/master
+}
